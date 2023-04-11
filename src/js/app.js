d3009b3c211b51a1ba8b97119fcfb0682ea01f5b
Kamarhinanton <Kamarhin_anton@ukr.net>
2023-04-11 11:37:12 +0300

    feat: add preloader

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,10 @@
 import {addClassHeader} from "./modules/addClassToHeader.js";
 import {showSubmenu} from "./modules/showSubmenu.js";
+import {preloader} from "./modules/preloader.js";
 
 
 window.addEventListener('load', () => {
+  preloader()
   addClassHeader()
   showSubmenu(document.querySelectorAll('.submenu-questions'));
 });

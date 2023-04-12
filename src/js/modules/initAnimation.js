893de42964dfc5fcd8ff7e679d451f465de4bedf
Kamarhinanton <Kamarhin_anton@ukr.net>
2023-04-12 11:27:36 +0300

    feat: add animation testimonials

--- src/js/modules/initAnimation.js
+++ src/js/modules/initAnimation.js
@@ -1,4 +1,7 @@
 import {gsap} from "gsap";
+import {ScrollTrigger} from "gsap/ScrollTrigger.js"
+
+gsap.registerPlugin(ScrollTrigger)
 
 export const initAnimation = () => {
   const animTopText = document.querySelector(".hero__content_description>h1");
@@ -6,35 +9,42 @@ export const initAnimation = () => {
   const animTopTopic = document.querySelector(".hero__content_description>p");
   const circle = document.querySelector(".circle");
   const topImg = document.querySelector(".hero__content_img");
-  const timeLine = gsap.timeline();
+  const testimonials = document.querySelector(".testimonials__list");
 
-  if (animTopText) {
-    gsap.matchMedia().add("(min-width: 568px)", () => {
-      timeLine.fromTo(animTopText, {
-        y: 50,
-        opacity: 0
-      }, {
-        opacity: 1,
-        y: 0
-      }, "period-1")
-        .to(animTopButton, {opacity: 1}, "period-2")
-        .to(animTopTopic, {opacity: 1}, "period-2")
-        .fromTo(circle, {
-            width: "42.1rem",
-            height: "42.1rem",
-          }, {
-            width: "62.1rem",
-            height: "62.1rem"
-          }, "period-1"
-        )
-        .fromTo(topImg, {
-            x: "20rem",
-            y: "20rem",
-          }, {
-            x: 0,
-            y: 0
-          }, "period-1"
-        )
+  gsap.matchMedia().add("(min-width: 568px)", () => {
+    gsap.timeline().fromTo(animTopText, {
+      y: 50,
+      opacity: 0
+    }, {
+      opacity: 1,
+      y: 0
+    }, "period-1")
+      .to(animTopButton, {opacity: 1}, "period-2")
+      .to(animTopTopic, {opacity: 1}, "period-2")
+      .fromTo(circle, {
+          width: "42.1rem",
+          height: "42.1rem",
+        }, {
+          width: "62.1rem",
+          height: "62.1rem"
+        }, "period-1"
+      )
+      .fromTo(topImg, {
+          x: "20rem",
+          y: "20rem",
+        }, {
+          x: 0,
+          y: 0
+        }, "period-1"
+      )
+
+    gsap.from(testimonials, {
+      scrollTrigger: {
+        trigger: testimonials,
+      },
+      y: "20rem",
+      opacity: 0
     })
-  }
+
+  })
 }
